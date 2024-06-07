ad54d16baf0ab9e3a31b3c784774314d29abdec2
SilentCraftsman <ggambier72@gmail.com>
2024-06-07 16:07:18 +0200

    La Navbar fini (ou presque)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Logo from "../assets/Logo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img src={Logo} alt="Logo" style={{ width: "220px", height: "80px" }} />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaSquareXTwitter />
+        <FaInstagram />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
