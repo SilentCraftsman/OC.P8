095dcdaa44a742cb4bd42260e093ff0f73fc6b99
SilentCraftsman <ggambier72@gmail.com>
2024-06-07 12:49:05 +0200

    Assets et projects folders + import from Google font Inter

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline"> Hello tout le monde!</h1>
+      <h1 className="text-3xl font-bold underline"> Hello tout le monde !</h1>
     </div>
   );
 };
