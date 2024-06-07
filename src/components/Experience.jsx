9d8f63a3a7347f89f48aab9b1de5e888b5420b88
SilentCraftsman <ggambier72@gmail.com>
2024-06-07 19:34:20 +0200

    Création de Experience.jsx

---
+++ src/components/Experience.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Experience = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+      accusantium repellendus hic obcaecati non consectetur debitis quasi a in
+      earum quis expedita cumque quaerat modi deserunt, quam beatae corrupti
+      praesentium.
+    </div>
+  );
+};
+
+export default Experience;
