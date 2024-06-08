c5313242f2d8132afb1b59bf10142720e3937cbb
SilentCraftsman <ggambier72@gmail.com>
2024-06-08 15:00:29 +0200

    Animation complète de Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,44 @@
 import { CONTACT } from "../constants";
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-20">
-      <h2 className="my-10 text-center text-4xl">Contact</h2>
-      <div className="text-center tracking-tighter">
-        <p className="my-4"> {CONTACT.address}</p>
-        <p className="my-4">{CONTACT.phoneNo}</p>
-        <a href="#" className="border-b">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -80 }}
+        transition={{ duration: 0.5 }}
+        className="my-10 text-center text-4xl">
+        Contact
+      </motion.h2>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className="text-center tracking-tighter">
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.8 }}
+          className="my-4">
+          {CONTACT.address}
+        </motion.p>
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 1.1 }}
+          className="my-4">
+          {CONTACT.phoneNo}
+        </motion.p>
+        <motion.a
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 1.4 }}
+          href="#"
+          className="border-b my-4 inline-block">
           {CONTACT.email}
-        </a>
-      </div>
+        </motion.a>
+      </motion.div>
     </div>
   );
 };
