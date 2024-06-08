cee3975876e8a5c8558f8bdc8c9dc630844d9857
SilentCraftsman <ggambier72@gmail.com>
2024-06-08 14:01:26 +0200

    Animation complète de About.jsx et correction de Techno.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,18 @@ import { motion } from "framer-motion";
 const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl">À propos</h2>
-      <div className="flex flex-wrap">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl">
+        À propos
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap">
         <motion.div
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: -100 }}
@@ -29,7 +39,7 @@ const About = () => {
             <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
           </div>
         </motion.div>
-      </div>
+      </motion.div>
     </div>
   );
 };
--- src/components/Techno.jsx
+++ src/components/Techno.jsx
@@ -4,6 +4,20 @@ import { SiMongodb } from "react-icons/si";
 import { DiRedis } from "react-icons/di";
 import { FaNodeJs } from "react-icons/fa";
 import { BiLogoPostgresql } from "react-icons/bi";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const Techno = () => {
   const iconStyles =
@@ -14,12 +28,53 @@ const Techno = () => {
       <h1 className="my-20 text-center text-4xl">Technologies</h1>
       <div className="flex justify-center">
         <div className="flex items-center gap-4">
-          <RiReactjsLine className={iconStyles} />
-          <TbBrandNextjs className={iconStyles} />
-          <SiMongodb className={iconStyles} />
-          <DiRedis className={iconStyles} />
-          <FaNodeJs className={iconStyles} />
-          <BiLogoPostgresql className={iconStyles} />
+          <motion.div
+            variants={iconVariants(2.5)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+            <RiReactjsLine className={iconStyles} />
+          </motion.div>
+
+          <motion.div
+            variants={iconVariants(3)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+            <TbBrandNextjs className={iconStyles} />
+          </motion.div>
+
+          <motion.div
+            variants={iconVariants(3.5)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+            <SiMongodb className={iconStyles} />
+          </motion.div>
+
+          <motion.div
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+            <DiRedis className={iconStyles} />
+          </motion.div>
+
+          <motion.div
+            variants={iconVariants(5)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+            <FaNodeJs className={iconStyles} />
+          </motion.div>
+
+          <motion.div
+            variants={iconVariants(6)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+            <BiLogoPostgresql className={iconStyles} />
+          </motion.div>
         </div>
       </div>
     </div>
