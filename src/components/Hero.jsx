edcf463e4ec23cfa5a89572451d5a2e2998be410
SilentCraftsman <ggambier72@gmail.com>
2024-06-08 12:47:37 +0200

    Animation du h1 de Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/Photo_profil_OC.png";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -7,12 +8,16 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+            <motion.h1
+              initial={{ x: -100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 2 }}
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
               John Doe
-            </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+            </motion.h1>
+            <motion.span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
               Dev Web
-            </span>
+            </motion.span>
             <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
               {HERO_CONTENT}
             </p>
