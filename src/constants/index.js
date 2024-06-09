ea6de22fd6d56dc04c5541b421c9c008b6854fc6
SilentCraftsman <ggambier72@gmail.com>
2024-06-09 15:22:28 +0200

    Mise en place de la partie Experience.jsx

--- src/constants/index.js
+++ src/constants/index.js
@@ -15,32 +15,19 @@ Explorez mon portfolio pour voir mes réalisations et contactez-moi pour discute
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "Décembre 2023 à Juin 2024 ",
+    role: "Développeur Web",
+    company: "Openclassrooms",
+    description: `Dans ma formation de Développeur Web, j'ai appris à concevoir, créer et maintenir des sites web dynamiques en utilisant des langages tels que HTML, CSS, JavaScript et des frameworks comme React. Avec une expertise pratique en développement back-end utilisant NodeJS, Express et MongoDB, je suis prêt pour une carrière dans ce domaine passionnant et en constante évolution.`,
+    technologies: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "React",
+      "Express",
+      "MongoDB",
+      "NodeJS",
+    ],
   },
 ];
 
