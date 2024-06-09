1c0a592b7f5a6ea16eff1200f317b4572fc46ddf
SilentCraftsman <ggambier72@gmail.com>
2024-06-09 15:08:15 +0200

    Mise en place concrète des logos de Techno.jsx

--- src/components/Techno.jsx
+++ src/components/Techno.jsx
@@ -1,9 +1,10 @@
-import { RiReactjsLine } from "react-icons/ri";
-import { TbBrandNextjs } from "react-icons/tb";
+import { SiGithub } from "react-icons/si";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { TbBrandJavascript } from "react-icons/tb";
+import { FaReact } from "react-icons/fa";
 import { SiMongodb } from "react-icons/si";
-import { DiRedis } from "react-icons/di";
-import { FaNodeJs } from "react-icons/fa";
-import { BiLogoPostgresql } from "react-icons/bi";
+import { FaNode } from "react-icons/fa";
 import { motion } from "framer-motion";
 
 const iconVariants = (duration) => ({
@@ -25,18 +26,61 @@ const Icon = ({ IconComponent, duration }) => (
     initial="initial"
     animate="animate"
     className="rounded-2xl border-4 border-neutral-800 p-4">
-    <IconComponent className="text-7xl md:text-6xl lg:text-7xl text-white-800 border-4 border-purple-200 rounded-2xl" />
+    <IconComponent
+      className="text-7xl md:text-6xl lg:text-7xl rounded-2xl"
+      style={{
+        color: getIconColor(IconComponent),
+        backgroundColor: getBackgroundColor(IconComponent),
+      }}
+    />
   </motion.div>
 );
 
+const getIconColor = (IconComponent) => {
+  if (IconComponent === SiGithub) {
+    return "#181717"; // Couleur associée à GitHub
+  } else if (IconComponent === FaHtml5) {
+    return "#E34F26"; // Couleur associée à HTML5
+  } else if (IconComponent === FaCss3Alt) {
+    return "#1572B6"; // Couleur associée à CSS3
+  } else if (
+    IconComponent === TbBrandJavascript ||
+    IconComponent === FaReact ||
+    IconComponent === FaNode
+  ) {
+    return "#F7DF1E"; // Couleur associée à JavaScript, React et Node.js
+  } else if (IconComponent === SiMongodb) {
+    return "#47A248"; // Couleur associée à MongoDB
+  } else {
+    return "#000000"; // Couleur par défaut
+  }
+};
+
+const getBackgroundColor = (IconComponent) => {
+  if (
+    IconComponent === SiGithub ||
+    IconComponent === FaHtml5 ||
+    IconComponent === FaCss3Alt ||
+    IconComponent === TbBrandJavascript ||
+    IconComponent === FaReact ||
+    IconComponent === FaNode ||
+    IconComponent === SiMongodb
+  ) {
+    return "#ffffff";
+  } else {
+    return "transparent";
+  }
+};
+
 const Techno = () => {
   const icons = [
-    { component: RiReactjsLine, duration: 2.5 },
-    { component: TbBrandNextjs, duration: 3 },
-    { component: SiMongodb, duration: 3.5 },
-    { component: DiRedis, duration: 4 },
-    { component: FaNodeJs, duration: 5 },
-    { component: BiLogoPostgresql, duration: 6 },
+    { component: SiGithub, duration: 2.5 },
+    { component: FaHtml5, duration: 3 },
+    { component: FaCss3Alt, duration: 3.5 },
+    { component: TbBrandJavascript, duration: 4 },
+    { component: FaReact, duration: 5 },
+    { component: SiMongodb, duration: 6 },
+    { component: FaNode, duration: 7 },
   ];
 
   return (
